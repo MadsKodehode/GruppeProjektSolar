7b6adad9de4b2d545dfc8c05ce26b83a08ddfea0
MadsKodehode <mads.kodehode@gmail.com>
2022-10-14 10:59:23 +0200

    Added hamburger menu for contactpage and cleaned up index.js file

--- contact.js
+++ contact.js
@@ -3,14 +3,9 @@ const bar2 = document.querySelector(".menu-bar2");
 const bar3 = document.querySelector(".menu-bar3");
 const navMenu = document.getElementById("nav-wrap");
 
-const btn = document.getElementById("btn");
 function changeFunction() {
   bar1.classList.toggle("transform");
   bar2.classList.toggle("transform");
   bar3.classList.toggle("transform");
   navMenu.classList.toggle("active");
-}
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
 }
